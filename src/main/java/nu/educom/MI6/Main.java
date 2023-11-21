8e36ff9f4c64c8a780d7cd78e539812e3be16731
coolgamery <ianberg@hotmail.nl>
2023-11-21 12:28:27 +0100

    1ste console opdracht versie 1

--- src/main/java/nu/educom/MI6/Main.java
+++ src/main/java/nu/educom/MI6/Main.java
@@ -1,7 +1,54 @@
 package nu.educom.MI6;
+import java.util.Scanner;
+import java.util.ArrayList;
 
-public class Main {
+public class Main
+{
   public static void main(String[] args) {
-    System.out.println("Hello World");
+    Scanner scanner = new Scanner(System.in);
+    ArrayList<String> blackList = new ArrayList<>();
+    ArrayList<String> loggedInList = new ArrayList<>();
+
+    while (true)
+    {
+      System.out.println("Voer je dienstnummer in:");
+      String input = scanner.nextLine();
+
+      if (input.matches("\\d+"))
+      {
+        if (input.length() == 1 || input.length() == 2)
+        {
+          input = String.format("%03d", Integer.parseInt(input));
+        }
+        try
+        {
+          int number = Integer.parseInt(input);
+          System.out.println("Wat is de geheime code?");
+
+          if (number >= 1 && number <= 956 && !blackList.contains(input) && !loggedInList.contains(input))
+          {
+            String input2 = scanner.nextLine();
+            if ("For ThE Royal QUEEN".equals(input2))
+            {
+              System.out.println("Je bent nu ingelogd agent: " + input + "!");
+              loggedInList.add(input);
+            }
+            else
+            {
+              System.out.println("Je wordt nu geblacklist");
+              blackList.add(input);
+            }
+          }
+          else
+          {
+            System.out.println("Ongeldig dienstnummer!");
+          }
+        }
+        catch (NumberFormatException e)
+        {
+          System.out.println("Ongeldig dienstnummer! exception");
+        }
+      }
+    }
   }
 }
